061d7111270bd0a72d4d91cfae7ff74e210d9ddb
wertylu <alfeziat@gmail.com>
2022-06-08 15:34:31 +0300

    hressively compressed lab3

--- Lab3/Main.py
+++ Lab3/Main.py
@@ -1,13 +1,3 @@
-import re as regex
-if __name__ == '__main__':
-    needed_list = list()
-    with open("server.log.2018-04-05") as input_file:
-        list_lines = [line.strip() for line in input_file.readlines()]
-    for line in list_lines:
-        line_needed = (regex.findall("WFLYSRV.*signal$", line))
-        if line_needed:
-            needed_list.append(line_needed)
-    for match in needed_list:
-        print(match)
-    counter = len(needed_list)
-    print(f"{counter} matches")
+from re import findall
+needed_list = [findall("WFLYSRV.*signal$", line) for line in [line.strip() for line in open("server.log.2018-04-05")] if findall("WFLYSRV.*signal$", line)]
+print(*needed_list, f"{len(needed_list)} matches", sep='\n')
